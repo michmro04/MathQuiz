d9e941d5c7e47569489a178d2c7c8c5fb34b2f90
Michal Mrowicki <mrg.mrowicki@gmail.com>
2026-07-16 12:45:16 +0200

    Enter handling, index of equation, delay before the next equation

--- MathQuiz/MainWindow.xaml.cs
+++ MathQuiz/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Media;
 
 namespace MathQuiz
@@ -7,37 +8,45 @@ namespace MathQuiz
     {
         private QuizEngine _quiz;
         private int _score = 0;
+        private int _indexOfEquation = 1;
 
         public MainWindow()
         {
             InitializeComponent();
 
             _quiz = new QuizEngine();
+            FeedbackText.Text = "";
 
             AskNewQuestion();
         }
 
         private void AskNewQuestion()
         {
+            FeedbackText.Text = "";
+
             _quiz.GenerateNewQuestion();
-            EquationText.Text = _quiz.CurrentEquation;
+            EquationText.Text = $"{_indexOfEquation}.  {_quiz.CurrentEquation}";
+            _indexOfEquation++;
+
             ScoreText.Text = $"Score = {_score}";
 
             AnswerInput.Text = ""; //clearing textbox for new answer
             AnswerInput.Focus(); //setting focus to the answer input box
         }
 
-        private void CheckButton_Click(object sender, RoutedEventArgs e)
+        private async void CheckButton_Click(object sender, RoutedEventArgs e)
         {
             string userInput = AnswerInput.Text;
 
-            if(int.TryParse(userInput, out int parsedAnswer))
+            if (int.TryParse(userInput, out int parsedAnswer))
             {
                 if (_quiz.CheckAnswer(parsedAnswer))
                 {
                     FeedbackText.Foreground = Brushes.Green;
                     FeedbackText.Text = "Correct!";
                     _score++;
+                    await Task.Delay(2000);
+
                     AskNewQuestion();
                 }
                 else if (!_quiz.CheckAnswer(parsedAnswer))
@@ -45,6 +54,7 @@ namespace MathQuiz
                     FeedbackText.Foreground = Brushes.Red;
                     FeedbackText.Text = $"Incorrect! Answer is {_quiz.CurrentAnswer}.";
                     _score--;
+                    await Task.Delay(1500);
                     AskNewQuestion();
                 }
             }
@@ -54,6 +64,14 @@ namespace MathQuiz
                 FeedbackText.Text = "Please enter a integer number.";
             }
 
+        }
+
+        private void AnswerInput_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if(e.Key == System.Windows.Input.Key.Enter)
+            {
+                CheckButton_Click(sender, e);
+            }
         }
     }
 }
