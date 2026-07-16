752799aa6b2b53df1646374baa668643c40b9488
Michal Mrowicki <mrg.mrowicki@gmail.com>
2026-07-16 12:04:55 +0200

    basic equation logic, UI changes - application works in very basic way

--- MathQuiz/MainWindow.xaml.cs
+++ MathQuiz/MainWindow.xaml.cs
@@ -1,24 +1,54 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
+﻿using System.Windows;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace MathQuiz
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private QuizEngine _quiz;
         public MainWindow()
         {
             InitializeComponent();
+
+            _quiz = new QuizEngine();
+            
+            AskNewQuestion();
+        }
+
+        private void AskNewQuestion()
+        {
+            _quiz.GenerateNewQuestion();
+            EquationText.Text = _quiz.CurrentEquation;
+
+            AnswerInput.Text = ""; //clearing textbox for new answer
+            AnswerInput.Focus(); //setting focus to the answer input box
+        }
+
+        private void CheckButton_Click(object sender, RoutedEventArgs e)
+        {
+            string userInput = AnswerInput.Text;
+
+            if(int.TryParse(userInput, out int parsedAnswer))
+            {
+                if (_quiz.CheckAnswer(parsedAnswer))
+                {
+                    FeedbackText.Foreground = Brushes.Green;
+                    FeedbackText.Text = "Correct!";
+                    AskNewQuestion();
+                }
+                else if (!_quiz.CheckAnswer(parsedAnswer))
+                {
+                    FeedbackText.Foreground = Brushes.Red;
+                    FeedbackText.Text = $"Incorrect! Answer is {_quiz.CurrentAnswer}.";
+                    AskNewQuestion();
+                }
+            }
+            else
+            {
+                FeedbackText.Foreground = Brushes.Orange;
+                FeedbackText.Text = "Please enter a integer number.";
+            }
+
         }
     }
 }
