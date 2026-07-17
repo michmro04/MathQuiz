039606213acfe6b22a4f8207ae39410334cf45de
Michal Mrowicki <mrg.mrowicki@gmail.com>
2026-07-17 09:21:17 +0200

    adding Pages of particular topics of Math and MainPage from which we can go to the next pages.

---
+++ MathQuiz/EquationsPage.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MathQuiz
+{
+    public partial class EquationsPage : Page
+    {
+        private QuizEngine _quiz;
+        private int _score = 0;
+        private int _indexOfEquation = 1;
+
+        public EquationsPage()
+        {
+            InitializeComponent();
+
+            _quiz = new QuizEngine();
+            FeedbackText.Text = "";
+
+            AskNewQuestion();
+        }
+        private void AskNewQuestion()
+        {
+            FeedbackText.Text = "";
+
+            _quiz.GenerateNewQuestion();
+            ExerciseText.Text = $"{_indexOfEquation}. Enter the solution.";
+            _indexOfEquation++;
+
+            ScoreText.Text = $"Score: {_score}";
+            EquationText.Text = $"{_quiz.CurrentEquation} =";
+
+            AnswerInput.Text = ""; //clearing textbox for new answer
+            AnswerInput.Focus(); //setting focus to the answer input box
+        }
+
+        private async void CheckButton_Click(object sender, RoutedEventArgs e)
+        {
+            string userInput = AnswerInput.Text;
+
+            if (int.TryParse(userInput, out int parsedAnswer))
+            {
+                if (_quiz.CheckAnswer(parsedAnswer))
+                {
+                    FeedbackText.Foreground = Brushes.LightGreen;
+                    FeedbackText.Text = "Correct!";
+                    _score++;
+                    await Task.Delay(1500);
+
+                    AskNewQuestion();
+                }
+                else if (!_quiz.CheckAnswer(parsedAnswer))
+                {
+                    FeedbackText.Foreground = Brushes.Red;
+                    FeedbackText.Text = $"Incorrect! Answer is {_quiz.CurrentAnswer}.";
+                    _score--;
+                    await Task.Delay(1500);
+                    AskNewQuestion();
+                }
+            }
+            else
+            {
+                FeedbackText.Foreground = Brushes.Orange;
+                FeedbackText.Text = "Please enter a integer number.";
+            }
+
+        }
+
+        private void AnswerInput_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == System.Windows.Input.Key.Enter)
+            {
+                CheckButton_Click(sender, e);
+            }
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+        }
+    }
+}
--- MathQuiz/MainWindow.xaml.cs
+++ MathQuiz/MainWindow.xaml.cs
@@ -6,73 +6,11 @@ namespace MathQuiz
 {
     public partial class MainWindow : Window
     {
-        private QuizEngine _quiz;
-        private int _score = 0;
-        private int _indexOfEquation = 1;
-
         public MainWindow()
         {
             InitializeComponent();
 
-            _quiz = new QuizEngine();
-            FeedbackText.Text = "";
-
-            AskNewQuestion();
-        }
-
-        private void AskNewQuestion()
-        {
-            FeedbackText.Text = "";
-
-            _quiz.GenerateNewQuestion();
-            ExerciseText.Text = $"{_indexOfEquation}. Enter the solution.";
-            _indexOfEquation++;
-
-            ScoreText.Text = $"Score: {_score}";
-            EquationText.Text = $"{_quiz.CurrentEquation} =";
-
-            AnswerInput.Text = ""; //clearing textbox for new answer
-            AnswerInput.Focus(); //setting focus to the answer input box
-        }
-
-        private async void CheckButton_Click(object sender, RoutedEventArgs e)
-        {
-            string userInput = AnswerInput.Text;
-
-            if (int.TryParse(userInput, out int parsedAnswer))
-            {
-                if (_quiz.CheckAnswer(parsedAnswer))
-                {
-                    FeedbackText.Foreground = Brushes.LightGreen;
-                    FeedbackText.Text = "Correct!";
-                    _score++;
-                    await Task.Delay(1500);
-
-                    AskNewQuestion();
-                }
-                else if (!_quiz.CheckAnswer(parsedAnswer))
-                {
-                    FeedbackText.Foreground = Brushes.Red;
-                    FeedbackText.Text = $"Incorrect! Answer is {_quiz.CurrentAnswer}.";
-                    _score--;
-                    await Task.Delay(1500);
-                    AskNewQuestion();
-                }
-            }
-            else
-            {
-                FeedbackText.Foreground = Brushes.Orange;
-                FeedbackText.Text = "Please enter a integer number.";
-            }
-
-        }
-
-        private void AnswerInput_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
-        {
-            if(e.Key == System.Windows.Input.Key.Enter)
-            {
-                CheckButton_Click(sender, e);
-            }
+            MainFrame.Navigate(new HomePage());
         }
     }
 }
